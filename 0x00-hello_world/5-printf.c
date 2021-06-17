8bdaf7ff9838fca8f37db3b030daf8d8660472a7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 14:16:29 -0500

    add back slash to instert new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+int main (void) {
 
-   printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-   return(0);
+return(0);
 }
