b7e0f0edbf230d2d9d8629c4a5bf3becd7783f39
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 14:09:25 -0500

    use command printf nstead of put

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-algo debe suceder aqui
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char mico[56];
+
+   strcpy(mico, "with proper grammar, but the outcome is a piece of art,");
+
+   printf(mico);
+
+   return(0);
+}
