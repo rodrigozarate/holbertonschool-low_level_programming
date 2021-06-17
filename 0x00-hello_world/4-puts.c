8aedc15712d8b69f6672cf581f05dc9de2f81bd8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 14:00:29 -0500

    first attempt to write a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-algo debe suceder aqui
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char vaca[15];
+
+   strcpy(vaca, "\"Programming is like building a multilingual puzzle");
+
+   puts(vaca);
+   
+   return(0);
+}
