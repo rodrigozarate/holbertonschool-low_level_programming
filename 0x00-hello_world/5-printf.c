15c99d97d7e8b3c273f8adc110ae193afb433b9f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 20:56:50 -0500

    adding defintion of function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,19 @@
+/**
+ * main - print a hard coded string
+ * ----
+ * no parameters
+ * Return: Zero value
+ * 17/06/2021
+ * Rodrigo Zárate Algecira
+ */
+
 #include <stdio.h>
 #include <string.h>
 
-int main (void) {
+int main(void)
+{
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 
 return(0);
 }
