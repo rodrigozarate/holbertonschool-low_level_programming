3dfcae1cfd494e5fc72950aebe851ba72cecf411
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 21:05:36 -0500

    remove spaces and add tabs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,12 +10,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+int main(void)
+{
 	char vaca[52];
 
 	strcpy(vaca, "\"Programming is like building a multilingual puzzle");
 
 	puts(vaca);
-   
+
 	return (0);
 }
