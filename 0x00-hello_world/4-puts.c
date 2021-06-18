184c3836bc234cb5c2435f76a635526714bda3bc
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-17 21:03:16 -0500

    format to betty complains

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
+/**
+ * main - print hard coded string using puts
+ * ----
+ * no parameters
+ * Return: Zero value
+ * 17/06/2021
+ * Rodrigo Zárate Algecira
+ */
+
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-   char vaca[52];
+int main(void) {
+	char vaca[52];
 
-   strcpy(vaca, "\"Programming is like building a multilingual puzzle");
+	strcpy(vaca, "\"Programming is like building a multilingual puzzle");
 
-   puts(vaca);
+	puts(vaca);
    
-   return(0);
+	return (0);
 }
