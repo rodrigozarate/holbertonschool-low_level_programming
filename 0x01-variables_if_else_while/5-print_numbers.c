d162cc178183c5f13356299a86dcb548aa9949bd
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-18 13:47:12 -0500

    numbers to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+* File: 5-print_numbers.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Friday 18 june 2021
+*/
+
+#include <stdio.h>
+
+/**
+* main - Prints single digits base 10
+* Return: zero
+*/
+
+int main(void)
+{
+int rza;
+
+	for (rza = 0; rza < 10; rza++)
+		printf("%d", rza);
+
+	printf("\n");
+
+	return (0);
+}
