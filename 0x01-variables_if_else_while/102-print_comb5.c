4e20faf2851951a0d871ce9f402b3dc9c27b8ca7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-18 23:54:12 -0500

    remove identation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,21 +19,21 @@ int main(void)
 int r1, r2, r3, r4, r5, r6;
 r1 = r2 = r3 = r4 = 48;
 
-	while (r4 < 58)
+while (r4 < 58)
+{
+r3 = 48;
+	while (r3 < 58)
 	{
-	r3 = 48;
-		while (r3 < 58)
+	r2 = 48;
+		while (r2 < 58)
 		{
-		r2 = 48;
-			while (r2 < 58)
+		r1 = 48;
+			while (r1 < 58)
 			{
-			r1 = 48;
-				while (r1 < 58)
+			r5 = (r4 * 10) + r3;
+			r6 = (r2 * 10) + r1;
+				if (r5 < r6)
 				{
-				r5 = (r4 * 10) + r3;
-				r6 = (r2 * 10) + r1;
-					if (r5 < r6)
-					{
 					putchar(r4);
 					putchar(r3);
 					putchar(' ');
@@ -43,15 +43,15 @@ r1 = r2 = r3 = r4 = 48;
 						break;
 						putchar(',');
 						putchar(' ');
-					}
-					r1++;
 				}
-				r2++;
+				r1++;
 			}
-			r3++;
+			r2++;
 		}
-		r4++;
+		r3++;
 	}
-	putchar('\n');
-	return (0);
+	r4++;
+}
+putchar('\n');
+return (0);
 }
