fa9566077c7f6b8be147fc40d1a73ca501191bd5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-18 21:44:11 -0500

    combination of digits program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+/*
+* File: 100-print_comb3.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Friday 18 june 2021
+*/
+
+#include <stdio.h>
+
+/**
+* main - Print the combinatios of two digits
+* Return: zero
+*/
+
+int main(void)
+{
+	int nm1, nm2;
+
+	for (nm1 = 0; nm1 < 10; nm1++)
+	{
+		for (nm2 = 0; nm2 < 10; nm2++)
+		{
+			putchar((nm1 % 10) + '0');
+			putchar((nm2 % 10) + '0');
+
+			if (nm1 == 9 && nm2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
