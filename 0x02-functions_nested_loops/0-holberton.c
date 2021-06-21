206d6f25d344cea2fc8f568a9ba38da9a8ee3e77
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-21 15:46:00 -0500

    add end line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,13 +4,26 @@
 * Date: Monday june 21 2021
 */
 
-#include <rza.h>
+#include "holberton.h"
 
 /**
 * main - Print holberton
 * Return: Zero
 */
+
 int main(void)
 {
+
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+
 return (0);
 }
