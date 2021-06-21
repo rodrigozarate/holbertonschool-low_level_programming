c62d906012dc927aca3a4a9386ebdcd20315371a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-21 14:07:11 -0500

    now on lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/*
+* File: 0-holberton.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Monday june 21 2021
+*/
+
+#include <rza.h>
+
+/**
+* main - Print holberton
+* Return: Zero
+*/
+int main(void)
+{
+return (0);
+}
