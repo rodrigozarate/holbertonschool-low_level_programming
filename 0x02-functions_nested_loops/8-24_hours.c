fcd246654bfc3b748ba1fe67427d2c3a3b6fc40a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-22 15:56:00 -0500

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+/*
+* File: 8-24_hours.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Tuesday june 22 2021
+*/
+
+#include "holberton.h"
+
+/**
+* jack_bauer - Print minutes in one day
+* Return: void
+*/
+void jack_bauer(void)
+{
+/* hour bucle */ 
+/* minute bucle */
+}
