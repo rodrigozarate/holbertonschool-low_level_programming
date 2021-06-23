94ef3789342b25e00dd5cce3df5de6d90e2265ae
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-23 09:04:59 -0500

    change quotes of vars

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 {
 int x, y, z;
 
-	for (x = '0'; x < '10'; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = '0'; y < '10'; y++)
+		for (y = 0; y < 10; y++)
 		{
 		z = x * y;
 		_putchar(z + '0');
