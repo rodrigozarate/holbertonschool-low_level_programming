2d64f88238e560aeb9d8d80ca35a25c2fc7149f9
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-22 23:53:45 -0500

    additioin

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+/*
+* File: 10-add.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Tuesday june 22 2021
+*/
+
+#include "holberton.h"
+
+/**
+* add - Computes add
+* Return: addition of int
+*/
+int add(int, int)
+{
+	int x, y, z;
+
+	scanf("%d%d", &x, &y);
+
+	z = x + y;
+
+	return (z);
+}
