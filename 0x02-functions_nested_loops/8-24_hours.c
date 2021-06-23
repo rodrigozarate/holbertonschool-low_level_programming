c59606b06b3c697a373278aca5b32244f950b0a2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-22 23:31:40 -0500

    change place of if

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,46 +12,35 @@
 */
 void jack_bauer(void)
 {
-
-int h1, h2, m1, m2;
-
+int h1, h2, h3, m1, m2;
 	for (h1 = 0; h1 < 3; h1++)
 	{
-		if(h1 < 2)
+		for (h2 = 0; h2 < 10; h2++)
 		{
-			for (h2 = 0; h2 < 10; h2++)
+			for (m1 = 0; m1 < 6; m1++)
 			{
-				for (m1 = 0; m1 < 6; m1++)
+				for (m2 = 0; m2 < 10; m2++)
 				{
-					for (m2 = 0; m2 < 10; m2++)
-					{
 					_putchar(h1 + '0');
+					if (h1 < 2)
+					{
 					_putchar(h2 + '0');
+					}
+					else if (h1 == 2 && h2 > 4)
+					{
+					_putchar(h3 + '0');
+					h3++;
+					}
 					_putchar(':');
 					_putchar(m1 + '0');
 					_putchar(m2 + '0');
 					_putchar('\n');
-					}
-				}
-			}
-		}
-		else
-		{
-			for (h2 = 0; h2 < 4; h2++)
-			{
-				for (m1 = 0; m1 < 6; m1++)
-                                {
-					for (m2 = 0; m2 < 10; m2++)
+					if (h3 > 4)
 					{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m1 + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
+					h3 = 0;
 					}
 				}
 			}
 		}
-	}	
+	}
 }
