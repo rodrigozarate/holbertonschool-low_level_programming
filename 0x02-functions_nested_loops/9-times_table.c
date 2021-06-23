5c3e4a19b6f1a22803e70c2a2d793dc852f21113
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-23 11:55:08 -0500

    new approach

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,25 @@
 */
 void times_table(void)
 {
-int x, y;
-char z;
+int x, y, z;
+int dg1, dg2;
+
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-		z = (x + '0') * (y + '0');
-		_putchar('z');
+		z = x * y;
+		if (z > 10)
+		{
+		dg1 = 255;
+		}
+		else
+		{
+		dg1 = z / 10;
+		}
+		dg2 = z % 10;
+		_putchar(dg1);
+		_putchar(dg2);
 		_putchar(255);
 		_putchar(',');
 		}
