0795bc354d94391469f0f4bd5b10a69edf166150
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-23 22:08:56 -0500

    change symbols

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,18 +21,19 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
-		printf("98");
+		printf("98\n");
 	}
-	if (n >= 98)
+	if (n > 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("98\n");
 	}
 	if (n == 98)
 	{
-		printf("98");
+		printf("98\n");
 	}
 }
