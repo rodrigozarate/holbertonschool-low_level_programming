9cf6a175604b0085d69593123ce24ca12edf83c5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-23 21:58:43 -0500

    remove extra var

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,21 +18,21 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ", n , n);
+			printf("%d, ", n);
 			n++;
 		}
-		printf('98');
+		printf("98");
 	}
 	if (n >= 98)
 	{
 		while (n > 98)
 		{
-			printf("%d, ", n , n);
+			printf("%d, ", n);
 			n--;
 		}
 	}
 	if (n == 98)
 	{
-		printf("%d, ", n , n);
+		printf("98");
 	}
 }
