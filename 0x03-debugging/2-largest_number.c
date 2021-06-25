df0aadcd2c9037bbd188041831765436a871b87f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-24 20:36:11 -0500

    add comparison

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c && b > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c && a > c)
 	{
 		largest = b;
 	}
