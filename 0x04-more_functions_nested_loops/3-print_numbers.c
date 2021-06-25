8710d2d6f2667860403a030d6fd15985f225baa0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-24 21:41:05 -0500

    number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_numbers - Numbers 0 to 9 followed by nl.
+*
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+_putchar('\n');
+}
