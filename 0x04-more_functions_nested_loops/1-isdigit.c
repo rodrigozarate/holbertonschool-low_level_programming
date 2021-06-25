4d13d276afac9728be46ba1ee200560928447a54
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-24 21:22:33 -0500

    fix style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
-  
+
 /**
- * _isdigit - Digit verificator
- * @c: integer
- * Return: 0 or 1 on success
- */
+* _isdigit - Digit verificator
+* @c: integer
+* Return: 0 or 1 on success
+*/
 int _isdigit(int c)
 {
-        if ((c >= 48) && (c <= 57))
-        {
-                return (1);
-        }
-        else
-                return (0);
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
