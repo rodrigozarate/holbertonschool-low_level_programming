bfb6b33dfd1a34f2b5e1742f6fae4f85d2bebada
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-28 15:10:56 -0500

    fix pus ione·

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,13 +17,13 @@ void print_rev(char *s)
 int x;
 int y;
 x = 0;
-        while (s[x] != '\0')
-        {
-                x++;
-        }
-	for (y = x; y > 0; y--)
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (y = (x + 1); y > 0; y--)
 	{
 		_putchar(s[y]);
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
