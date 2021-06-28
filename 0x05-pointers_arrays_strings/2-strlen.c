4f4b0577aacba475564e83f7f3a3d1ef46c8e025
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-28 12:57:29 -0500

    first try

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/*
+* 2-strlen.c
+* Auth: Rodrigo Zárate Algecira
+* Date: Monday 28 june 2021
+*/
+
+#include "holberton.h"
+
+/**
+* _strlen - Returns lenght of a string
+* @s: char
+* Return: lenght of string in s
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
