d6866fcb9c06125e346f0130500f2ca3179f9ce2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-28 16:08:21 -0500

    change place of var lrev and declar z

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,12 +16,13 @@ void rev_string(char *s)
 {
 int x;
 int y;
+int z = 0;
+char lrev = s;
 x = 0;
 	while (s[x] != '\0')
 	{
 		x++;
 	}
-	char lrev[x];
 	for (y = (x - 1); y >= 0; y--, z++)
 	{
 		lrev[z]=s[y];
