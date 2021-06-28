d540277ba8e8eb989667861a5f7a746641e6def1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-28 15:07:04 -0500

    add print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+/*
+* File: 4-print_rev.c
+* Author: Rodrigo Zárate Algecira
+* Date: Monday 28 june 2021
+*/
+
+#include "holberton.h"
+
+/**
+* print_rev - Prints a string in reverse.
+* @s: a string
+* Return: the string to stdout
+*/
+
+void print_rev(char *s)
+{
+int x;
+int y;
+x = 0;
+        while (s[x] != '\0')
+        {
+                x++;
+        }
+	for (y = x; y > 0; y--)
+	{
+		_putchar(s[y]);
+	}
+        _putchar('\n');
+}
