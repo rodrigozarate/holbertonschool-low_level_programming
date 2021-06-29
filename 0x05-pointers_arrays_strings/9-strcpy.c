e7383dc7b35ddd1eb76a07c7a973ed515fe86a42
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-29 14:15:19 -0500

    first attempt

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+/*
+* File: 9-strcpy.c
+* Author: Rodrigo Zárate Algecira
+* Date: Tuesday 29 june 2021
+*/
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strcpy - Copy the string pointed to buffer.
+* @dest: buffer
+* @src: an string
+* Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int x = 0;
+	while (src[x] != '\0')
+	{
+	/* count */
+		*dest[x] = src[x];
+	}
+	return(dest);
+}
