ef4ed0bb6f64d182b11c520d9aba7bedc768e411
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-28 23:52:59 -0500

    half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+/*
+* File: 7-puts_half.c
+* Author: Rodrigo Zárate Algecira
+* Date: Monday 28 june 2021
+*/
+
+#include "holberton.h"
+
+/**
+* puts_half - Prints last half of a string.
+* @str: a string
+* Return: 0
+*/
+
+void puts_half(char *str)
+{
+int x;
+int p = 0;
+int m = 0;
+char *y;
+x = 0;
+y = str;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	if (x % 2 != 0)
+	{
+	m = (x - 1) / 2;
+	}
+	else
+	{
+	m = x / 2;
+	}
+	y +=  m;
+	p = p + m;
+	while (p < x)
+	{
+	_putchar(*y);
+	p++;
+	y++;
+	}
+	_putchar('\n');
+}
