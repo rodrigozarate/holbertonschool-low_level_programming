7c49de57c43cbd0ea857925c74ee69c280236ede
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-29 13:08:11 -0500

    first try

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+/*
+* File: 8-print_array.c
+* Author: Rodrigo Zárate Algecira
+* Date: Tuesday 29 june 2021
+*/
+
+#include "holberton.h"
+
+/**
+* print_array - Prints n elemnts of an array.
+* @a: an array
+* @n: an int
+* Return: 0
+*/
+
+void print_array(int *a,int n)
+{
+int x;
+int y;
+x = 0;
+y = 0;
+	while (a[x] != '\0')
+	{
+	/* count */
+		if (y < n)
+		{
+			printf("%d, ", a[x]);	
+		} 
+		if (y == n)
+		{
+			printf("%d", a[x]);
+		}
+		x++;
+		y++;
+	}
+	_putchar('\n');
+}
