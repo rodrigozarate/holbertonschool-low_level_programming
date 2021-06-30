ed56b3181b69f6255402987740ca4f5d6e6dc487
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-06-29 23:35:42 -0500

    first try

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+/*
+* File: 100-atoi.c
+* Author: Rodrigo Zárate Algecira
+* Date: Tuesday 29 june 2021
+*/
+
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* atoi - Remove letters.
+* Return: void
+*/
+
+int _atoi(char *s)
+{
+int x = 48;
+int y = 57;
+int a = 0;
+int plus = 43;
+int less = 45; 
+/* if ascii > 48 && < 57 */
+	while(s[a])
+	{
+		if(s[a] > 48 && s[a] < 57)
+		{
+		putchar(s[a]);
+		}
+		else
+		{
+		return (0);
+		}
+	a++;
+	}
+}
