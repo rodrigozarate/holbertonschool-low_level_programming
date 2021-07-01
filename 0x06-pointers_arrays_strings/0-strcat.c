cea92e2e0cc16caa5d01fff39df4dfde5a16437b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-01 12:50:23 -0500

    pseudo code

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+/*
+* File: 0-strcat.c
+* Author: Rodrigo Zárate Algecira
+* Date: Thursday 01 july 2021
+*/
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strcat - Append scr to dest.
+* @dest: final string concatenated
+* @src: an string
+* Return: the pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+/* count first string */
+/* count second string */
+/* create a new string that can hold the two strings */
+/* copy value of string one, then string two */
+/* append null */
+return();
+}
