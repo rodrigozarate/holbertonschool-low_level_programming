fdd1cb45cf7efd037d774088198eefac4c62c0ee
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-01 13:52:22 -0500

    pseodicode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/*
+* File: 7-leet.c
+* Author: Rodrigo Zárate Algecira
+* Date: Thursday 01 july 2021
+*/
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* leet - encode to leet.
+* Return: the pointer to dest
+*/
+
+char *leet(char *)
+{
+/* walk string */
+/* test if character is on the list */
+/* if on the list change */
+/* else continue */
+/* append null */
+return();
+}
