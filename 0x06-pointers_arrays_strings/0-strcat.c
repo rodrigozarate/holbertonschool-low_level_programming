cabf17ca6034feb1f4a1d82905cc998dc2887211
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-01 20:54:14 -0500

    doing the copy cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,10 +16,29 @@
 
 char *_strcat(char *dest, char *src)
 {
+int x;
+int y;
+int z;
+
 /* count first string */
+	while (scr[x])
+	{
+		x++;
+	}
 /* count second string */
+	while (dest[y])
+	{
+		y++;
+	}	
 /* create a new string that can hold the two strings */
+
 /* copy value of string one, then string two */
+	for (i = y, j = 0; i < y + x; i++, j++)
+	{
+		dest[i] = src[j]; 
+	}
+	z = x + y + 1;
+	dest[z] = '\0';
 /* append null */
-return();
+return(dest);
 }
