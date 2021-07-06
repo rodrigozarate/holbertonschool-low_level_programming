fbedcdef222b1e8825ec0f15ac42082081c3ecd5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-05 20:51:25 -0500

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+/*
+* File: 1-memcpy.c
+* Author: Rodrigo Zárate Algecira
+* Date: Monday 06 july 2021
+*/
+
+#include "holberton.h"
+
+/**
+* _memset - Fills memory.
+* @dest: char
+* @src: char
+* @n: unsigned int
+* Return: the pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+	for (i = 0; i < n; i++)
+	{	
+		dest[i] = src[i];
+	}
+return (dest);
+}
