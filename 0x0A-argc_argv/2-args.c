bf9a5cdf7172c9db5bd9a8920e99034adc1e5ab1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-08 11:33:21 -0500

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+/*
+* File: 2-args.c
+* Author: Rodrigo Zárate Algecira
+* Date: Thursday 08 july 2021
+*/
+
+#include <stdio.h>
+
+/**
+* main - print all args
+* @argc: number of args
+* @argv: vector of args
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+int x;
+	for(x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+}
