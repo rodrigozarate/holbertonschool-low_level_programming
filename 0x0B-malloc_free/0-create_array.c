519f8798b1eb0ff2f26fb36c5d4c4782a8e62f11
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-13 13:38:12 -0500

    validate task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+/* 0-create_array.c - exercise to try malloc
+* Author: Rodrigo Zarate Algecira
+* Date: Juy 13 2021
+*/
+
+/**
+* create_array - Creates an array
+* @size: size of array
+* @c: character
+* Return: NULL or a pointer/Users/user/holbertonschool-low_level_programming/0x0B-malloc_free  
+*/
+
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+	return (NULL);
+	}
+}
