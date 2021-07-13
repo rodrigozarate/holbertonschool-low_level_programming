b451c20e605bb4b68e5440c4133bd01207016eb7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-13 13:48:14 -0500

    skeleton

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,19 @@
+/*
+* 1-strdup.c - exercise to try malloc
+* Author: Rodrigo Zarate Algecira
+* Date: Juy 13 2021
+*/
+
+#include "holberton.h"
+
+/**
+* _strdup - Creates an array
+* @size: size of array
+* @c: character
+* Return: NULL or a pointer
+*/
+
+char *_strdup(char *str)
+{
+
+}
