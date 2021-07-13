0f3f0259c89f4ab61f84995837603d39cd86fd73
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-13 14:51:23 -0500

    do the task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,9 @@
 * Date: Juy 13 2021
 */
 
+#include "holberton.h"
+#include <stdlib.h>
+
 /**
 * create_array - Creates an array
 * @size: size of array
@@ -14,8 +17,26 @@
 
 char *create_array(unsigned int size, char c)
 {
+int i;
+char *x;
+/*get the size of char */
+x = malloc(size * sizeof(char));
+
+	/* case size 0 */
 	if (size == 0)
 	{
 	return (NULL);
 	}
+	
+	/* case x  NULL */
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	/* create the array */
+	for (i = 0; i < size; i++)
+	{
+		x[i] = c;
+	}
+return (x);
 }
