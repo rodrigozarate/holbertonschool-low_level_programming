7a948af18ff411ec8b0c15f4bbfc7ed965598824
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-13 20:32:27 -0500

    do the malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,13 +7,43 @@
 #include "holberton.h"
 
 /**
-* _strdup - Creates an array
-* @size: size of array
-* @c: character
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: string
 * Return: NULL or a pointer
 */
 
 char *_strdup(char *str)
 {
+/* hold the new str */
+char *newstr;
+/* fo counters */
+int i;
+/* to get end position */
+int lengtstr = 0;
+	/* case: str= NULL */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/* walk the string */
+	for (i = 0; str[i]; i++)
+	{
+		lengtstr++;
+	}
+
+	newstr = malloc((lengtstr + 1) * sizeof(char));
+	/* malloc fail */
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		newstr[i] = str[i];
+	}
+	/* end of line */
+	newstr[lengtstr] = '\0';
+
+return (newstr);
 
 }
