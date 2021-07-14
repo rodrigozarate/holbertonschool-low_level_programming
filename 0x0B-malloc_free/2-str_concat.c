d391f8ea3a5dfed5e676637b78d44a9e512a836b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-13 21:53:22 -0500

    do the concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,19 +1,58 @@
 /*
-* 1-strdup.c - exercise to try malloc
+* 2-str_concat.c - exercise to join two strings using malloc
 * Author: Rodrigo Zarate Algecira
 * Date: Juy 13 2021
 */
 
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
-* _strdup - Creates an array
-* @size: size of array
-* @c: character
+* _str_concat - Join  tow strings
+* @s1: string one
+* @s2: string two
 * Return: NULL or a pointer
 */
 
-char *_strdup(char *str)
+char *_str_concat(char *s1, char *s2)
 {
+char *thereturnedpointer;
+int lengthone = 0;
+int lengthtwo = 0;
 
+int i, x;
+	while (s1[i])
+	{
+		i++;
+		lengthone++;
+	}
+	while (s2[i])
+	{
+		i++;
+		lengthtwo++;
+	}
+	if (s1 == NULL)
+	}
+		lengthone = 0;
+	}
+	if (s2 == NULL)
+	{
+		lengthtwo = 0;
+	}
+	x = lengthone + lengthtwo + 1;
+	thereturnedpointer = malloc(x * sizeof(char));
+	if (thereturnedpointer == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lengthone; i++)
+	{
+		thereturnedpointer[i] = s1[i];
+	}
+	for (i = 0; i < lengthtwo; i++;
+	{
+		thereturnedpointer[i + lengthone] = s2[i];
+	}
+	thereturnedpointer[lengthone + lengthtwo] = '\0';
+return (thereturnedpointer);
 }
