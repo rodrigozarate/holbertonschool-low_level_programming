82effafd988ff7430657e6ebd8e33e91bc0af90a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-19 16:44:24 -0500

    [CREATE] Dog do dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Make a dog
+ * @name: Self explanatory
+ * @age: Self explanatory
+ * @owner: Self explanatory
+ *
+ * Description: Create a dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
