6af247dcc1b90a2486936c2c2a866ffcab11270c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-19 21:08:49 -0500

    [CREATE]

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* init_dog - Function 
+* @d:pointer
+* @name:pointer
+* @age:float
+* @owner:pointer
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	/* condition */
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
