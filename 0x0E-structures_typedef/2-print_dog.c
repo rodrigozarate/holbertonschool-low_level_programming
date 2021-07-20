636cffeb723c677e5ff5464339796b6edc3a4f4e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-19 21:40:50 -0500

    [CREATE] dog print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,46 @@
+/*
+* 2-print_dog.c - Print struct dog
+* Author: Rodrigo Zárate Algecira
+* Date: 19, July 2021
+*/
+
+#include <stdio.h>
+#inlcude "dog.h"
+
+/**
+* print_dog - Print the dog
+* @d: Struct dog
+* Return: Null or dog
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return (0);
+	}
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+	if (d->age != NULL)
+	{
+		printf("Age: %.6f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+}
