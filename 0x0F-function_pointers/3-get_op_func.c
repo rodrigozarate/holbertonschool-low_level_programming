7fdba67ee18e40e749b19c011bc5a4c64f76ea99
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-21 23:55:12 -0500

    [CREATE]

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+* get_op_func - Get the operation
+* @s: operation
+*
+* Return: pointer
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
