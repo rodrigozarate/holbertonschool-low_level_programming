08edc70011273b749d05c8b758a66198ee9cb221
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-23 09:39:57 -0500

    [CREATE] header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
