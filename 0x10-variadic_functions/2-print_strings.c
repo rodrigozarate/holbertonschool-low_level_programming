833e11f8fd6c7f53fe6826a0b754a6e09ff49eaf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-23 16:25:29 -0500

    [CREATE]

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,49 @@
+/*
+* 2-print_strings.c - Print string
+* Author: Rodrigo Zárate Algecira
+* Date: July 23, 2021
+*/
+
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_strings - Print strings thats it...
+* @n: const unsigned int
+* @separator: The separator between numbers
+* Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+/* declare unsigned to compare an count */
+unsigned int i;
+
+	/* call structure */
+	va_list theList;
+
+	/* init */
+	va_start(theList, n);
+	/* walk the list */
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(theList, char *))
+		{
+			printf("%s", va_arg(theList, char *));
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (separator && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	/* endit */
+	va_end(theList);
+
+printf("\n");
+}
