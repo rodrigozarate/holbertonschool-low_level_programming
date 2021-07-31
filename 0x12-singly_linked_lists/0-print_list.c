8291f4a89d4593e55ce7b7c099bd5b42463a396b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-30 21:14:16 -0500

    [CREATE]

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+/*
+* 0-print_list.c - Print list
+* Author: Rodrigo Zárate Algecira
+* Date: July 30, 2021
+*/
+
+#include "lists.h"
+
+/**
+* print_list - Prints elements in list
+* @h: pointer to const list
+* Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+
+int i;
+const list_t *dt = h;
+size_t howmany = 0;
+/* walk list */
+	for (i = 0; dt; i++)
+	{
+		if (dt->str == NULL)
+		{
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", dt->len, dt->str);
+		}
+	dt = dt->next;
+	howmany++;
+	}
+return (howmany);
+}
