71e5109a73abc443d289a4e77e8e3d8e92b207b5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-02 11:34:01 -0500

    [CREATE]

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+/*
+* 0-print_listint.c - Print list
+* Author: Rodrigo Zárate Algecira
+* Date: July 23, 2021
+*/
+
+#include "lists.h"
+
+/**
+* print_listint - Print list of ints
+* @h: const pointer
+* Return: size_t
+*/
+
+size_t print_listint(const listint_t *h)
+{
+
+}
