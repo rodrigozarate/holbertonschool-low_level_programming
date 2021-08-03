253dea72153d90386818f0c159f4231ad1bc0c22
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-02 23:31:05 -0500

    [CREATE]

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,46 @@
+/*
+* 3-add_nodeint_end.c - Add node at the end of list
+* Author: Rodrigo Zárate Algecira
+* Date: August 02, 2021
+*/
+
+#include "lists.h"
+
+/**
+* add_nodeint_end - Add node int at end of list
+* @n: const
+* @head: pointer to pointer
+* Return: Address or NULL
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *newnode, *initnode;
+
+initnode = *head;
+newnode = malloc(sizeof(litsint_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	/* set value */
+	newnode->n;
+	/* mark end */
+	newnode->next = NULL;
+	/* List is empty */
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	/* walk list */
+	while (initnode->next)
+	{
+		initnode = initnode->next;
+	}
+/* last next in node is NULL */
+/* reach the end then add node */
+initnode->next = newnode;
+return (newnode);
+}
