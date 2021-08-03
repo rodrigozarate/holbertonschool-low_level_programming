b0e19678123924c30470d2160159fab85e6e26c0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-03 17:48:43 -0500

    [CREATE]

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,41 @@
+/*
+* 6-pop_listint.c - pop list
+* Author: Rodrigo Zárate Algecira
+* Date: August 03, 2021
+*/
+
+#include "lists.h"
+
+/**
+* free_listint - free list
+* @head: Pointer
+* Return: void
+*/
+
+int pop_listint(listint_t **head);
+{
+	listint_t *nodenext, *verbatim;
+	int i;
+	/* copy info */
+	verbatim = head;
+	/* set i */
+	if (verbatim == NULL)
+	{
+		i = 0;
+	}
+	else
+	{
+		i = verbatim->n;
+	}
+	/* walk list */
+	while (verbatim)
+	{
+		/* assign */
+		nodenext = verbatim->next;
+		/* free item by item */
+		free(verbatim);
+		/* move to next */
+		verbatim = nodenext;
+	}
+return(i);
+}
