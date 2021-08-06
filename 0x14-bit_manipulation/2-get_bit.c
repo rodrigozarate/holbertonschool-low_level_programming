d358baedbda7205946b8dac0e9fdd0697c973e3c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-05 22:56:21 -0500

    [CREATE]

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* get_bit - get bit at index
+* @n: unsigned long int
+* @index: insigned int
+* Return: int
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	/* index is not in range */
+	if (index > (sizeof(n) * 8))
+		return (-1);
+/* shift to index */
+n >> index;
+	/* compare to get 1 */
+	if ((n & 1) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
