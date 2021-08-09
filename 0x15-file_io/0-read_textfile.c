605a8da583e2cc47a9b3267b389a56e021c7fae4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-09 16:38:53 -0500

    [CREATE]

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+/*
+* 0-read_textfile.c - Reads the content of given file
+* Author: Rodrigo Zárate Algecira
+* Date: August 09, 2021
+*/
+
+#include "main.h"
+
+/**
+* read_textfile - Reads the content of given file
+* @filename: char filename
+* @letters: size_t max letters to be written
+* Return: number of printed chars
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+if (filename == NULL)
+	return (0);
+if (reading fails)
+	return (0);
+if (write fails)
+	return (0);
+/* read chars until given size */
+/* send char to POSIX OUTPUT */
+/* if end is reached before given size stop */
+
+/* return number of chars */
+}
