7bdbafdc04f7cf7bbd292c812bfea2395d5eaaa1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-10 12:29:58 -0500

    [FIX] End the loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,38 +25,29 @@ char buffer[1024];
 int filefrom, fileto, readfrom, writeto, canclose;
 
 	if (anum != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
-	/* read the file of origin */
+
 	filefrom = open(aval[1], O_RDONLY);
 	if (filefrom == -1)
-	{
 		dprintf(rze, "Error: Can't read from file %s\n", aval[1]), exit(98);
-	}
-	/* open file to */
+
 	fileto = open(aval[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileto == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", aval[2]), exit(99);
-	}
+
 	readfrom = read(filefrom, buffer, 1024);
 	if (readfrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", aval[1]);
-		exit(98);
-	}
+		dprintf(rze, "Error: Can't read from file %s\n", aval[1]), exit(98);
+
 	while (readfrom > 0)
 	{
-		/* yucanread then copy */
 		writeto = write(fileto, buffer, readfrom);
 		if (writeto == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", aval[2]);
-			exit(99);
-		}
+			dprintf(rze, "Error: Can't write to %s\n", aval[2]), exit(99);
+		readfrom = read(filefrom, buffer, 1024);
+		if (readfrom == -1)
+			dprintf(rze, "Error: Can't read from file %s\n", aval[1]), exit(98);
 	}
-	/* cant close or then fail */
 	canclose = close(filefrom);
 	if (canclose == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filefrom), exit(100);
@@ -64,6 +55,6 @@ int filefrom, fileto, readfrom, writeto, canclose;
 	canclose = close(fileto);
 	if (canclose == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileto), exit(100);
-/* All ok */
+
 return (0);
 }
