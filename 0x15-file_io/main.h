cb9767e377c96e44b6b98f75c8c7cf017a6edd14
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-09 21:50:58 -0500

    [UPDATE]

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,13 @@
 #ifndef FILESIO_H
 #define FILESIO_H
 
+#include <sys/stat.h>
+#include <sys/ttypes.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
