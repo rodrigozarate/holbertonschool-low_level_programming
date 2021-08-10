0f4263e53b819de049a23ff65e0f71421b42971a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-09 23:51:06 -0500

    [CREATE]

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,43 @@
+/*
+* 3-cp.c - COPY file
+* Author: Rodrigo Zárate Algecira
+* Date: August 09, 2021
+*/
+
+#include "main.h"
+
+/**
+* main - copy the content and perms of a file
+* @anum: the name of teh file
+* @aval: The text
+* Return: 0 if ok
+* 97 if arg number is wrong
+* 98 if file to copy from does not exist
+* 99 if copy file can not be created
+* 100 if cant close
+*/
+
+int main(int anum, char **aval)
+{
+/* char buffer[1024]; */
+int filefrom, fileto, readfrom, writeto;
+
+	if (anum != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	/* read the file of orogin */
+	filefrom = open(aval[1], O_RDONLY);
+	if (filefrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
+	/* open file to */
+	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fileto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+/* ok */
+return (0);
+}
