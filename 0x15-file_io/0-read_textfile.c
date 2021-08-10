331482e0bbf75766ae716250450dcb6027c6c2b8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-09 21:54:20 -0500

    [UPDATE]

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,6 +7,18 @@
 #include "main.h"
 
 /**
+* freewilli - free mem
+* @themem: the malloc
+* Return: always 0
+*/
+
+int freewilli(char *themem)
+{
+	free(themem);
+	return (0);
+}
+
+/**
 * read_textfile - Reads the content of given file
 * @filename: char filename
 * @letters: size_t max letters to be written
@@ -15,15 +27,37 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == NULL)
-	return (0);
-if (reading fails)
-	return (0);
-if (write fails)
-	return (0);
-/* read chars until given size */
-/* send char to POSIX OUTPUT */
-/* if end is reached before given size stop */
+char *alm;
+int filetoread;
+ssize_t rfile = 0;
+ssize_t wfile = 0;
 
+	if (filename == NULL)
+		return (0);
+	/* space in mem */
+	alm = malloc(sizeof(char) * letters);
+	if (alm == NULL)
+		return (0);
+	/* open the file */
+	filetoread = open(filename, O_RDONLY);
+	if (filetoread == -1)
+	{
+		freewilli(alm);
+	}
+	/* read chars until given size */
+	rfile = read(filetoread, alm, letters);
+	if (rfile == -1)
+	{
+		freewilli(alm);
+	}
+	wfile = write(STDOUT_FILENO, alm, rfile);
+	if (wfile == -1)
+	{
+		freewilli(alm);
+	}
+free(alm);
+/* close the file */
+close(filetoread);
 /* return number of chars */
+return (wfile);
 }
