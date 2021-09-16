df529ef6cfef21bcd044973f66361b89ec5417b8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-16 10:31:03 -0500

    [CREATE]

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+/*
+* 0-print_dlistint.c - Prints node in double linked list
+* Author: Rodrigo Zárate Algecira
+* Date: September 16, 2021
+*/
+
+#include "lists.h"
+
+/**
+* print_dlistint - Prints node in double linked list
+* @h: pointer
+* Return: Int numbers of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+/* Traverse nodes for each node sum */
+return(sum);
+}
