a93654cf6a9fe4b1c6867527b77b7f0c5145ebe1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-16 11:08:34 -0500

    [UPDATE]

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,6 +4,8 @@
 * Date: September 16, 2021
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -15,5 +17,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 /* Traverse nodes for each node sum */
-return(sum);
+	int i;
+	size_t sum = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		sum++;
+	}
+return (sum);
 }
