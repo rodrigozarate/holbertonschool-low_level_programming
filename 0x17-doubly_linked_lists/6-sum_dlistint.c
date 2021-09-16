a3191e9c10477c36540c53de760ab27b60c8a82c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-16 14:23:20 -0500

    [UPDATE]

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,3 +1,17 @@
+/*
+* 6-sum_dlistint.c - Return the sum of nodes
+* Author: Rodrigo Zárate Algecira
+* Date: September 16, 2021
+*/
+
+#include "lists.h"
+
+/**
+* sum_dlistint - Return the sum of nodes
+* @head: pointer
+* Return: int
+*/
+
 int sum_dlistint(dlistint_t *head)
 {
 int sum = 0;
