bfbaab17f87d020a55095ff4d1952bc4efa921f3
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-16 11:36:05 -0500

    [CREATE]

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,39 @@
+/*
+* 2-add_dnodeint.c - adds a new node at the beginning of a list
+* Author: Rodrigo Zárate Algecira
+* Date: September 16, 2021
+*/
+
+#include "lists.h"
+
+/**
+* add_dnodeint - Add node in double linked list
+* @head: pointer
+* @n: value of node
+* Return: new node 
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+{
+	/* find head and insert */
+	/* cretae new node */
+	dlistint_t *newnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+	/* goes wrong */
+	if (newnode == NULL)
+		return (NULL);
+	/* set head */
+	newnode->prev = NULL;
+	/* point to prev head */
+	newnode->next = *head;
+	/* set value */
+	newnode->n = n;
+
+	/* head is valid */
+	if (*head != NULL)
+		(*head)->prev = newnode;
+	/* reset pointer head */
+	*head = newnode;
+	return (newnode);
+}
