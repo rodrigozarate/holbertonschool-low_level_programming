c60a336ca8b32f5d70c25adf698083f293b8339b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-16 15:12:00 -0500

    [CREATE]

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,9 @@
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	/* check range */
+	/* traverse list to match index */
+	/* get next AND Next-next */
+	/* get prev AND Prev->prev */
+	/* disconnect by updating */
+	/* next to next->next AND prev to prev->prev */
+}
