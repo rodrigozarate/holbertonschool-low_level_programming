1fdcf8866997b4d3dd609418fdd8d66e744ae028
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-17 23:24:25 -0500

    [UPDATE]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,15 +16,61 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode;
-	int i;
-	for (i = 0; i < idx and *h != NULL; i++)
+	unsigned int count = 0;
+	dlistint_t *firstnode, *newnode;
+	unsigned int lenght = node_len(h);
+
+	/* if cero insert at head */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	firstnode = *h;
+	/* check for range */
+	while (firstnode)
 	{
-		/* check for range */
-		/* if out of range exit */
-		/* if cero insert at head */
-		/* if equal end insert at end */
-		/* get h->next and h->next->next */
-		/* point prev to next AND next to Next->next */
+		if (count == idx - 1)
+			break;
+		else if (count < idx - 1 && firstnode == NULL)
+			return (NULL)
+		firstnode = firstnode->next;
+		count++;
 	}
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	/* add at the end */
+	if (lenght == idx)
+		return (add_dnodeint_end(h, n));
+	else if (lenght < idx)
+		return (NULL)
+
+	/* get here connect */
+	firstnode->next->prev = newnode;
+	newnode->next = firstnode->next;
+	firstnode->next = newnode;
+	newnode->prev = firstnode;
+	newnode->n = n;
+	return (newnode);
+}
+
+/**
+* node_len - Count nodes
+* @node: double pointer
+* Return: int
+*/
+
+node_len(dlistint_t **node)
+{
+	unsigned int length = 0;
+	dlistint_t *firstnode;
+
+	firstnode = *node;
+	while (firstnode != NULL)
+	{
+		length += 1;
+		firstnode = firstnode->next;
+	}
+	return (length);
 }
