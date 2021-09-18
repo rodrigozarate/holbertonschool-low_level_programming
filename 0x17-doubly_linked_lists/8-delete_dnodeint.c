37094ec751b8f03cd66d4fb280fd6e9e2eb35c59
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-17 23:53:47 -0500

    [UPDATE]

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,60 @@
+
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - Delete node at index
+* @head: double pointer
+* @index: int
+* Return: -1 if error or 1 on succed
+*/
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	/* check range */
-	/* traverse list to match index */
-	/* get next AND Next-next */
-	/* get prev AND Prev->prev */
-	/* disconnect by updating */
-	/* next to next->next AND prev to prev->prev */
+	dlistint_t *firstnode;
+	unsigned int i;
+	unsigned int lenght = node_len(head);
+
+	firstnode = *head;
+	/* no node content */
+	if (*head == NULL)
+		return (-1);
+	/* delete at start */
+	if (index == 0)
+	{
+		firstnode = firstnode->next;
+		if (!firstnode)
+			return (-1);
+	}
+	if (lenght - 1 == index)
+	{
+		firstnode->prev->next = NULL;
+		free(firstnode);
+		/* EXITO */
+		return (1);
+	}
+	firstnode->prev->next = firstnode->next;
+	firstnode->next->prev = firstnode->prev;
+	free(firstnode);
+	/* EXITO */
+	return (1);
+}
+
+/**
+* node_len - Count nodes
+* @node: double pointer
+* Return: int
+*/
+
+unsigned int node_len(dlistint_t **node)
+{
+	unsigned int length = 0;
+	dlistint_t *firstnode;
+
+	firstnode = *node;
+	while (firstnode != NULL)
+	{
+	length += 1;
+	firstnode = firstnode->next;
+	}
+	return (length);
 }
