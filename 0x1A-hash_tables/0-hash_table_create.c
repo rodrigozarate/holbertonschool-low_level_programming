1ec00bf7e4db6184d1c06453607f9572c5dedc16
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-08 10:10:14 -0500

    [CREATE]

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+/*
+* 0-hash_table_create.c - create Hash table
+* Author: Rodrigo Zárate Algecira
+* Date: October 8, 2021
+*/
+
+/**
+* hash_table_create - create Hash table
+* @size: unsigned long int
+* Return: pointer or NULL
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+return(pointer);
+else
+return(NULL);
+}
