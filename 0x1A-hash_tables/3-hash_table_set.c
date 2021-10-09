760ae469ffb4a3326672e8a3c1f0b4c05ba01957
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-08 22:13:10 -0500

    [UPDATE]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,7 +4,19 @@
 * Date: October 8, 2021
 */
 
+#include "hash_tables.h"
+
+/**
+* hash_table_set - set the hash table
+* @ht: structure
+* @key: char pointer
+* @value: char value
+*/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
+	/* key cannot be empty */
+	/* if collition first node */
+	/* value strdup */
+return (0);
 }
