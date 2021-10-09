e2ffa8fd55374f282966f0db76d263a7215c34d2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-08 20:42:47 -0500

    [UPDATE]

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,21 @@
 * Date: October 8, 2021
 */
 
+#include "hash_tables.h"
+
+/**
+* key_index - return key index
+* @key: char
+* @size: long int
+* Return: index
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int hash;
+	unsigned long int index;
 
+	hash = hash_djb2(key);
+	index = hash % size;
+
+return (index);
 }
