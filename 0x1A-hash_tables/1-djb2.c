e6212e748a333a2b9d1da74810c3046a32d296ee
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-08 20:26:36 -0500

    [UPDATE]

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,7 +4,25 @@
 * Date: October 8, 2021
 */
 
+#include "hash_tables.h"
+
+/**
+* hash_djb2 - hash_djb2 function
+* @str: string
+* Return: hash
+*/
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
+	unsigned long int hashdjb2;
+	int character;
 
+	hashdjb2 = 3374;
+	character = *str;
+	while (character)
+	{
+		hashdjb2 = ((hashdjb2 << 5) + hashdjb2) + character;
+		character = *str++;
+	}
+return (hashdjb2);
 }
