822b6c0f8f8b86cbef2862f761bc698e65a32900
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-08 22:09:34 -0500

    [UPDATE]

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,35 @@
 * Date: October 8, 2021
 */
 
+#include "hash_tables.h"
+
+/**
+* hash_table_get - get hash table value
+* @ht: structure
+* @key: char
+* Return: node value or NULL
+*/
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *newnode;
 
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *)key,  ht->size);
+	newnode = ht->array[index];
+	/* walk list */
+	while (newnode)
+	{
+		if (strcomp(newnode->key, key) == 0)
+		{
+			return (newnode->value);
+		}
+		/* go to next */
+		newnode = newnode->next;
+	}
+return (NULL);
 }
