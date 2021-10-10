065804f653d237edde910c1a10f16957b33b1baa
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-09 22:59:49 -0500

    [UPDATE]

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,7 +4,29 @@
 * Date: October 8, 2021
 */
 
+#include "hash_tables.h"
+
+/**
+*
+*
+*
+*/
+
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *node;
 
+	if (!ht || !ht->array || !ht->size)
+	{
+		return;
+	}
+	for ()
+	{
+		while ()
+		{
+			node = ht->array[i]->next;
+		}
+	}
+	free(ht);
 }
