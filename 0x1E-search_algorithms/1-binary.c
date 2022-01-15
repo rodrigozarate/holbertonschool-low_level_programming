c6980238bbfcdee260817409e897f9f10cdbaf9a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-14 20:31:20 -0500

    [CREATE]

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,8 @@
+// determine the middle of the array
+// test value agains middle
+// if lower
+// discard greater part and start process again
+// if greater
+// discard lower part and start process again
+// if match found return index
+// else return -1
